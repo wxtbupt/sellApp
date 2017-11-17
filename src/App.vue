1b813779b63d716dac7f790715175da2257db42b
wangxiaotong <wxt_bupt@163.com>
2017-11-17 20:40:06 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <div id="app">
+    <v-header></v-header>
+    <div class="tab">
+      <div class="tab-item">商品</div>
+      <div class="tab-item">评论</div>
+      <div class="tab-item">商家</div>
+    </div>
+    <div class="content">
+      content
+    </div>
+
+  </div>
+</template>
+
+<script>
+  import header from './components/header/header.vue';
+  export default {
+    components:{
+      v-header: v-header
+    }
+  };
+
+</script>
+
+<style lang="stylus" rel="stylesheet/stylus">
+  #app
+    .tab
+      display:flex
+      width: 100%
+      height: 40px//设计稿上面是80，但dpr=2，所以设为40px
+      line-height: 40px
+      .tab-item
+        flex:1
+
+
+
+</style>
---
+++ src/components/header/header.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="header">
+
+  </div>
+</template>
+
+<script>
+  export default {
+    components:{}
+  };
+</script>
+
+
+<style lang="stylus" rel="stylesheet/stylus">
+
+</style>
